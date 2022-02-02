55bcdccf23c10be534ba61d731a6f49ea827174d
Piotrsh <98518542+Piotrsh@users.noreply.github.com>
2022-02-02 19:13:29 +0100

    thrid commit

---
+++ src/com/company/Main2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main2{
+
+    public static void main(String[] args) {
+
+        System.out.println("Podaj wiek psa w latach");
+        Scanner entry = new Scanner(System.in);
+        double firstAgeDog = entry.nextInt();
+        int firstMonthDog = entry.nextInt();
+
+        double secondAgeDog = 0;
+
+        secondAgeDog =  16*Math.log(firstAgeDog)+31;
+        System.out.format("%.0f%n", secondAgeDog );
+    }
+}
